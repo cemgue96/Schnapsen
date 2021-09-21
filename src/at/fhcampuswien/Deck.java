8210c212f6304e158823b57a1d9d48af28584e1c
Tobias Gahleitner <tobias@web.erei.at>
2021-09-21 08:48:22 +0200

    Begin structuring - added Card and Deck class + minor functionality

---
+++ src/at/fhcampuswien/Deck.java
@@ -0,0 +1,38 @@
+package at.fhcampuswien;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import static java.util.Map.entry;
+
+public class Deck {
+
+    public static final List<String> COLOURS =
+            new ArrayList<>(Arrays.asList("Kreuz", "Pik", "Karo", "Herz"));
+    public static final Map<String, Integer> VALUES =
+            Map.ofEntries(
+                    entry("Ass", 11),
+                    entry("Zehner", 10),
+                    entry("König", 4),
+                    entry("Dame", 3),
+                    entry("Bube", 2));
+
+    private List<Card> cards = new ArrayList<>();
+
+    public Deck() {
+        _generateDeck();
+    }
+
+    public void shuffle(){
+        //TODO implement shuffle
+    }
+    private void _generateDeck(){
+        for (String color : COLOURS){
+            for (Map.Entry<String, Integer> value : VALUES.entrySet()){
+                Card card = new Card(color, value);
+                cards.add(card);
+            }
+        }
+    }
+}
