0a2bb83f87dd0e4b2a40e28bf89c93e26f72abd2
c1810536031 <patrick.weigl@stud.fh-campuswien.ac.at>
2021-10-08 14:36:05 +0200

    implemented print text function in GameUI

--- src/at/fhcampuswien/GameUI.java
+++ src/at/fhcampuswien/GameUI.java
@@ -5,4 +5,10 @@ public class GameUI {
     public static void printAtoutCard(Card atoutCard){
         System.out.println("The atout card is: " + atoutCard.getColor() + " " + atoutCard.getValue().getKey());
     }
+
+    public static void printText(String text) {
+        System.out.println(text);
+        System.out.print(System.lineSeparator());
+    }
 }
+
