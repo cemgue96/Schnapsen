2554acfa0e6d0af351dd613c194773ebd82f9368
Sebi <sebastian.podschlapp@gmail.com>
2021-10-08 11:44:19 +0200

    added player name in ArtificialPlayer class

--- src/at/fhcampuswien/ArtificialPlayer.java
+++ src/at/fhcampuswien/ArtificialPlayer.java
@@ -3,8 +3,8 @@ package at.fhcampuswien;
 import java.util.List;
 
 public class ArtificialPlayer extends Player {
-    public ArtificialPlayer(List<Card> handCards) {
-        super(handCards);
+    public ArtificialPlayer(List<Card> handCards, String playerName) {
+        super(handCards, playerName);
     }
 
     //TODO: implement algorithm on which an artificalPlayer generates
