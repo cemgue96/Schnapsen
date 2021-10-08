e395f853eb916c0cd5abf474ff69a69f8c287d26
Sebi <sebastian.podschlapp@gmail.com>
2021-10-08 11:43:18 +0200

    added player name in HumanPlayer class

--- src/at/fhcampuswien/HumanPlayer.java
+++ src/at/fhcampuswien/HumanPlayer.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class HumanPlayer extends Player {
 
-    public HumanPlayer(List<Card> handCards) {
-        super(handCards);
+    public HumanPlayer(List<Card> handCards, String playerName) {
+        super(handCards, playerName);
     }
 
     //TODO: override chooseCard function like printing playedCards, cards in hand and atout and let user select which card to chose
