b21e43b686891681b53b3b2778f9cd52145001e1
Ferdi <ferdinand.unger@icu.at>
2021-10-08 14:08:27 +0200

    Print Chosen Card

--- src/at/fhcampuswien/GameUI.java
+++ src/at/fhcampuswien/GameUI.java
@@ -5,4 +5,9 @@ public class GameUI {
     public static void printAtoutCard(Card atoutCard){
         System.out.println("The atout card is: " + atoutCard.getColor() + " " + atoutCard.getValue().getKey());
     }
+
+    public static void printChosenCard(String playerName, Card card) {
+        System.out.println(playerName + " played the card: " + card.getColor() + card.getValue().getKey());
+    }
+
 }
