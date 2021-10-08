b6e79b6fa6e9603d77f8ed5caefd36e4743bb9ed
Emel <emel.can@stud.fh-campuswien.ac.at>
2021-10-08 14:15:56 +0200

    Added printing of Gamepoints

--- src/at/fhcampuswien/GameUI.java
+++ src/at/fhcampuswien/GameUI.java
@@ -5,4 +5,9 @@ public class GameUI {
     public static void printAtoutCard(Card atoutCard){
         System.out.println("The atout card is: " + atoutCard.getColor() + " " + atoutCard.getValue().getKey());
     }
+
+
+    public static void printPoints(String playerName, int points) {
+        System.out.println(playerName + " gathered " + points + " points!");
+    }
 }
